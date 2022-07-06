6d9d041ae923dc875dbc612fdaf1d6f4e9e7bdbf
piotrekd00 <92211538+piotrekd00@users.noreply.github.com>
2022-07-06 19:57:06 +0200

    changed path to be more flexible

--- scripts.py
+++ scripts.py
@@ -3,21 +3,23 @@ import click
 import os
 from CsvSource import CsvSource
 from TxtSource import TxtSource
-
+from pathlib import Path
 
 def get_files():
     txt_path = []
     csv_path = []
+    curr_path = Path(__file__).parent.resolve()
+    email_path = str(curr_path)+'/emails'
 
-    if "emails" not in os.listdir("."):
+    if "emails" not in os.listdir(curr_path):
         print('Emails directory was not found! Exiting...')
         sys.exit()
     else:
-        for file in os.listdir("./emails"):
+        for file in os.listdir(email_path):
             if file.endswith(".txt"):
-                txt_path.append(os.path.join("emails", file))
+                txt_path.append(os.path.join(email_path, file))
             elif file.endswith(".csv"):
-                csv_path.append(os.path.join("emails", file))
+                csv_path.append(os.path.join(email_path, file))
 
         txt_files = [TxtSource(path, 'txt') for path in txt_path]
         csv_files = [CsvSource(path, 'csv') for path in csv_path]
